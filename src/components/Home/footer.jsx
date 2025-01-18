02f91cc5f19d035908869f4483d53d0ea51b9b61
John-Mukhwana <johnbradill67@gmail.com>
2025-01-18 21:48:03 +0300

    update footer copyright year and brand name

--- src/components/Home/footer.jsx
+++ src/components/Home/footer.jsx
@@ -3,7 +3,7 @@ const Footer=()=>{
   return(
     <footer className="bg-blue-800 dark:bg-gray-700 text-white py-4 mt-auto ">
       <div className="container mx-auto text-center">
-      <p>&copy; 2024 <span className="text-yellow-400 font-bold">Drive</span> <span className="text-white">And</span> <span className="text-yellow-400 font-bold">Dive</span>. All rights reserved.</p>
+      <p>&copy; 2025  <span className="text-yellow-400 font-bold">Eventia</span>. All rights reserved.</p>
       </div>
 
     </footer>
