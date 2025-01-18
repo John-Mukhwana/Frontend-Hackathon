f8891c7b7e4848ac500a45109f1dae01776ea361
John-Mukhwana <johnbradill67@gmail.com>
2025-01-18 18:54:49 +0300

    added Footer component to HomePage; include footer content and styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Home/footer"
 import Intro from "../components/Home/intro"
 import Navbar from "../components/Home/navbar"
 import WhyChooseUs from "../components/Home/whyUs"
@@ -14,6 +15,7 @@ const HomePage = () => {
            <div>
             <WhyChooseUs/>
            </div>
+           <Footer/>
         </div>
     )
 }
