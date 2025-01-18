e5ec259a02340922c01e1751082990bbf2ad52a4
John-Mukhwana <johnbradill67@gmail.com>
2025-01-19 00:12:03 +0300

    fix: update welcome message in Intro component for clarity

--- src/components/Home/intro.jsx
+++ src/components/Home/intro.jsx
@@ -20,7 +20,7 @@ const Intro = () => {
 
       {/* Intro Content */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white p-4">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">  Welcome to Eventia, your premier event management system.</h1>
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">  Welcome to Eventia, your premier event manager.</h1>
         <p className="text-lg md:text-2xl mb-6 max-w-2xl">
         Seamlessly organize, manage, and execute events with ease and efficiency. Experience unparalleled tools designed to make your events unforgettable.
         </p>
