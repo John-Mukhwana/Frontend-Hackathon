99018df0caa5ad080b9dd31b39ae3e797736a1da
John-Mukhwana <johnbradill67@gmail.com>
2025-01-19 14:32:38 +0300

    added required validation to name and email fields in SignUp form

--- src/components/Home/SignUp.jsx
+++ src/components/Home/SignUp.jsx
@@ -97,8 +97,10 @@ const RegisterForm = () => {
                 id="name"
                 name="name"
                 value={formData.name}
+                required
                 onChange={handleChange}
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                
               />
             </div>
 
@@ -110,6 +112,7 @@ const RegisterForm = () => {
                 name="email"
                 value={formData.email}
                 onChange={handleChange}
+                required
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
               />
             </div>
@@ -122,6 +125,7 @@ const RegisterForm = () => {
                 name="contact"
                 value={formData.contact}
                 onChange={handleChange}
+                required
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
               />
             </div>
