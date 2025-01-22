171ecc9319f3572abcc74a4afa80f1b6f3974c85
John-Mukhwana <johnbradill67@gmail.com>
2025-01-22 15:06:11 +0300

    updated copyright year in footer component to 2025

--- src/components/Home/footer.jsx
+++ src/components/Home/footer.jsx
@@ -3,7 +3,7 @@ const Footer=()=>{
   return(
     <footer className="bg-blue-800 dark:bg-gray-700 text-white py-4 mt-auto ">
       <div className="container mx-auto text-center">
-      <p>&copy; 2024  <span className="text-yellow-400 font-bold">Eventia</span>. All rights reserved.</p>
+      <p>&copy; 2025  <span className="text-yellow-400 font-bold">Eventia</span>. All rights reserved.</p>
       </div>
 
     </footer>
