99338c36eae36aff216fa7169a4cec24d43f8cb5
John-Mukhwana <johnbradill67@gmail.com>
2025-01-22 08:24:29 +0300

    updatedcurrency in EventDetails component from USD to KES

--- src/components/userComponents/EventDetails.jsx
+++ src/components/userComponents/EventDetails.jsx
@@ -40,7 +40,7 @@ const EventDetail = ({ event }) => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          amount: event.price * 100, // Amount in cents
+          amount: event.price/129 * 100, // Amount in cents
           currency: 'usd',
           userId: user.id, // Ensure you're sending userId
         }),
